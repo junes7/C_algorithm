71a1f6ee9ea17fdb062efa13ddaaff87ef63ca4f
junes7 <2junes@naver.com>
2021-07-06 04:31:51 +0900

    solve that print hello and helloworld problems by using printf function

---
+++ 1001.c
@@ -0,0 +1,9 @@
+// 출력하기 01
+// printf()를 이용해 다음 언어를 출력하시오.
+
+# include <stdio.h>
+
+int main() {
+    printf("Hello");
+    return 0;
+}
---
+++ 1002.c
@@ -0,0 +1,9 @@
+// 출력하기 02
+// 이번에는 공백()을 포함한 문장을 출력한다.
+// Hello World (대소문자에 주의한다.)
+# include <stdio.h>
+
+int main() {
+    printf("Hello World");
+    return 0;
+}
