49f905529bdc274eee0e1efd6251b3073896b7ec
junes7 <2junes@naver.com>
2021-07-05 12:43:40 +0900

    solve that print double quotation mark and special characters with printf function

---
+++ CodeUp/C_language_basic100/1005.c
@@ -0,0 +1,9 @@
+// 출력하기 05
+// 이번에는 큰따옴표(double quotation mark)가 포함된 출력문을 연습해보자.
+// "Hello World" (단, 큰따옴표도 함께 출력한다.)
+# include <stdio.h>
+
+int main() {
+    printf("\"Hello World\"");
+    return 0;
+}
