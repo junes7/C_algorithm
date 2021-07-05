37726566479bebb3be492d58bcc20f618452ef48
junes7 <2junes@naver.com>
2021-07-05 12:11:18 +0900

    solve that print new line and print single quotation mark problems by using print method

---
+++ 1003.c
@@ -0,0 +1,12 @@
+// 출력하기 03
+// 이번에는 줄을 바꿔 출력하는 연습문을 연습해보자.
+// 다음과 같이 줄을 바꿔 출력해야 한다.
+// Hello
+// World
+// (두 줄에 걸쳐 줄을 바꿔 출력)
+# include <stdio.h>
+
+int main() {
+    printf("Hello\nWorld");
+    return 0;
+}
