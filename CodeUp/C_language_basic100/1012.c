a606723b563484436b9ce4542f3b3d0a06ae7722
junes7 <2junes@naver.com>
2021-07-06 08:02:14 +0900

    solve that print floating point number after input and print two integers after input by using scanf function and printf function

---
+++ CodeUp/C_language_basic100/1012.c
@@ -0,0 +1,11 @@
+// 실수 1개 입력받아 그대로 출력하기
+// 실수형(float)으로 변수를 선언하고 그 변수에 실수값을 저장한 후 저장되어
+// 있는 실수값을 출력해보자.
+# include <stdio.h>
+
+int main() {
+    float f;
+    scanf("%f", &f);
+    printf("%f", f);
+    return 0;
+}
