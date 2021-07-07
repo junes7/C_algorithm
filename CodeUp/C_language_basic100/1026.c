3dc56090efa8cdf46fdd7aa5d266f47e779b4656
junes7 <2junes@naver.com>
2021-07-07 13:18:49 +0900

    solve that print one integer out separately and print minute after input hour/minute/second problems by using 1d array, for loop, and scanf method

---
+++ CodeUp/C_language_basic100/1026.c
@@ -0,0 +1,16 @@
+// 시분초 입력받아 분만 출력하기
+// print minute after input hour/minute/second
+// 입력되는 시:분:초 에서 분만 출력해보자.
+// int h, m, s;
+// scanf("%d:%d:%d", &h, &m, &s); 를 실행하면 콜론을 사이에 둔
+// 형식으로 입력되어, h, m, s에 각각 정수값만 저장된다.
+// 시 분 초가 시:분:초 형식으로 입력된다. | 분만 출력한다.
+// method 1
+# include <stdio.h>
+
+int main() {
+    int h, m, s;
+    scanf("%d:%d:%d", &h, &m, &s);
+    printf("%d", m);
+    return 0;
+}
