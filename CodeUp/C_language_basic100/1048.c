839150b8c9b5c378612fb46ef72f126eabeed0e0
junes7 <2junes@naver.com>
2021-07-09 09:55:18 +0900

    solve that print one integer multiplied by two and print as a power of two iin one time problems by using shift operator

---
+++ CodeUp/C_language_basic100/1048.c
@@ -0,0 +1,15 @@
+// 한 번에 2의 거듭제곱 배로 출력하기
+// print as a power of two in one time
+// 정수 2개(a, b)를 입력받아 a를 2^b배 곱한 값으로 출력해보자.
+// 0 <= a <= 10, 0 <= b <= 10
+// 예를 들어 1 3 이 입력되면 1을 2^3(8)배 하여 출력한다.
+// int a=1, b=10;
+// printf("%d", a << b); //2^10 = 1024 가 출력된다.
+# include <stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d", a << b);
+    return 0;
+}
