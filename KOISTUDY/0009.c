184b17c1b90f9e8c889b3ba13ea9f85513cad942
junes7 <2junes@naver.com>
2021-08-04 11:23:44 +0900

    review how to print sentence and one integer with scanf, printf function

---
+++ KOISTUDY/0009.c
@@ -0,0 +1,17 @@
+// 출력하기 09
+// 출력문 연습의 마지막 문제이다.(** 이 문제는 건너 뛰어도 된다.)
+// (생각과 시도를 많이 해야하는 문제들은 한 두 문제씩 넘겼다가 나중에
+// 풀어보면 된다.)
+// 이번에는 다음과 같은 C프로그램의 소스파일을 출력해보자.
+// (들여쓰기 부분은 공백(' ') 4개이다.)
+// #include<stdio.h>
+// main()
+// {
+//     printf("Hello World\n");
+// }
+// 위 프로그램을 출력하시오.
+# include <stdio.h>
+int main() {
+    printf("#include<stdio.h>\nmain()\n{\n    printf(\"Hello World\\n\");\n}");
+    return 0;
+}
