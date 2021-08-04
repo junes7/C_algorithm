136e3e1eb128082af521fe5aa2cadd14d6bdb79a
junes7 <2junes@naver.com>
2021-08-04 11:35:09 +0900

    review how to print one input string and real number with different data type, and scanf, printf funciton

---
+++ KOISTUDY/0011.c
@@ -0,0 +1,8 @@
+// 문자 한 개 입력받아 그대로 출력하기
+// print one input string
+# include <stdio.h>
+int main() {
+    char x;
+    scanf("%c",&x);
+    printf("%c",x);
+}
---
+++ KOISTUDY/0012.c
@@ -0,0 +1,11 @@
+// 실수 한 개 입력받아 그대로 출력하기
+// print one input real number
+// 실수형(float)로 변수를 선언하고 그 변수에 실수값을 저장한 후 저장되어
+// 있는 실수값을 출력해보자.
+#include <stdio.h>
+int main() {
+    float f;
+    scanf("%f",&f);
+    printf("%.3f",f);
+    return 0;
+}
