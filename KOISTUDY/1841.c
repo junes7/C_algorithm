e523e90a2af998f4d9f08018b94f77c911d0c77e
junes7 <2junes@naver.com>
2021-08-13 09:03:04 +0900

    review how to print string and integer through function

---
+++ KOISTUDY/1841.c
@@ -0,0 +1,13 @@
+// 함수로 hello 문자열 출력하기
+// print hello through function
+// hello를 출력하시오. 단, 함수형 문제이므로 함수 f()만 작성하시오.
+# include <stdio.h>
+void f();
+void f(){
+    printf("hello");
+    return;
+}
+int main() {
+    f();
+    return 0;
+}
---
+++ KOISTUDY/1842.c
@@ -0,0 +1,13 @@
+// 함수로 123 값 출력하기
+// print 123 value through function
+// 123 을 출력하시오. 단, 함수형 문제이므로 함수 f()만 작성하시오.
+# include <stdio.h>
+void f();
+void f(){
+    printf("%d",123);
+    return;
+}
+int main(){
+    f();
+    return 0;
+}
