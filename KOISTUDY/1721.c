98429d0f9d67ac5c27e1d7ea500a79ae4f04c82c
junes7 <2junes@naver.com>
2021-08-19 08:05:48 +0900

    solve fill 2D array with zigzag pattern problems by using for loop, 2D array, if, else statement, and scanf, printf function

---
+++ KOISTUDY/1721.c
@@ -0,0 +1,38 @@
+// 2차원 배열 지그재그 채우기 5
+// fill 2D array with zigzag pattern
+# include <stdio.h>
+int d[110][110]={};
+int main() {
+    int n,m,i,j,t=1;
+    scanf("%d %d",&n,&m);
+    for(i=n;i>0;i--){
+        if(n%2==1){
+            if(i%2==1){
+                for(j=m;j>0;j--){
+                    d[i][j]=t++;
+                }
+            }else{
+                for(j=1;j<=m;j++){
+                    d[i][j]=t++;
+                }
+            }
+        }else{
+            if(i%2==0){
+                for(j=m;j>0;j--){
+                    d[i][j]=t++;
+                }
+            }else{
+                for(j=1;j<=m;j++){
+                    d[i][j]=t++;
+                }
+            }
+        }
+    }
+    for(i=1;i<=n;i++){
+        for(j=1;j<=m;j++){
+            printf("%d ",d[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
