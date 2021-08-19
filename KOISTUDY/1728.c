4ff3514d2d08309186da70957066cd435df802d1
junes7 <2junes@naver.com>
2021-08-19 11:44:57 +0900

    solve fill 2D array with diagonal pattern problems by using nested for loop, 2D array, if, else statement, and scanf, printf function

---
+++ KOISTUDY/1728.c
@@ -0,0 +1,23 @@
+// 2차원 배열 빗금 채우기 4
+// fill 2D array with diagonal pattern
+# include <stdio.h>
+int d[110][110]={};
+int main() {
+    int n,m,i,j,k,t=1;
+    scanf("%d %d",&n,&m);
+    for(i=1;i<=n+m;i++){
+        for(j=m;j>0;j--){
+            for(k=1;k<=n;k++){
+                if(k+(m-j)==i)
+                   d[k][j]=t++; 
+            }
+        }
+    }
+    for(i=1;i<=n;i++){
+        for(j=1;j<=m;j++){
+            printf("%d ",d[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
