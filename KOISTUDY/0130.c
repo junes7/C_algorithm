19a3c285e23bdc06799f8cf80b2157b7581dc316
junes7 <2junes@naver.com>
2021-08-20 11:29:17 +0900

    solve get nth fibonacci number and fibonacci sequence problems by using recursive function, if, else statement, and scanf, printf function

---
+++ KOISTUDY/0130.c
@@ -0,0 +1,26 @@
+// n번째 피보나치 수 구하기
+// get nth fibonacci number
+// 피보나치 수는 다음과 같이 정의된다.
+// f1 = 1, f2 = 1
+// fn = fn-1 + fn-2 ( n > 2 )
+// 한 정수를 입력받아서 fn을 구하는 프로그램을 작성하시오.
+# include <stdio.h>
+long long int d[100]={};
+long long int fibo(int n){
+    if(n<=2){
+        d[n]=1;
+        return d[n];
+    }
+    else if(d[n]!=0)
+        return d[n];
+    else{
+        d[n]=fibo(n-1)+fibo(n-2);
+        return d[n];
+    }
+}
+int main() {
+    int n;
+    scanf("%d",&n);
+    printf("%lld",fibo(n));
+    return 0;
+}
