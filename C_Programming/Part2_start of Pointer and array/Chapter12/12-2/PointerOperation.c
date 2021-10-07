41ab6042fb8486a854066fefa5b880a77275fd19
junes7 <2junes@naver.com>
2021-10-07 09:03:28 +0900

    study how to define and apply pointer operator

---
+++ C_Programming/Part2_start of Pointer and array/Chapter12/12-2/PointerOperation.c
@@ -0,0 +1,11 @@
+// num1:130, num2:70
+#include<stdio.h>
+int main(void){
+    int num1=100, num2=100;
+    int *pnum;
+    pnum=&num1;
+    (*pnum)+=30;
+    pnum=&num2;
+    (*pnum)-=30;
+    printf("num1:%d, num2:%d\n",num1,num2);
+}
