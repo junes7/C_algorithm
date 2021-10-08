3aae82cb77d2c11f177e0f4c66613fa044862939
junes7 <2junes@naver.com>
2021-10-09 07:09:49 +0900

    study how to define and use Stirng pointer array

---
+++ C_Programming/Part2_start of Pointer and array/Chapter13/StringArray.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(void){
+    char *strArr[3]={"simple","string","array"};
+    printf("%s\n",strArr[0]);
+    printf("%s\n",strArr[1]);
+    printf("%s\n",strArr[2]);    
+}
