448cc6300c42319a27d2c95b14e5a28f1e6b6988
junes7 <2junes@naver.com>
2021-10-10 08:28:36 +0900

    study how to swap three values by using call by reference

---
+++ C_Programming/Part2_start of Pointer and array/Chapter14/Question/14-1/Q2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void Swap3(int *ptr1,int *ptr2,int *ptr3){
+    int temp=*ptr3;
+    *ptr3=*ptr2;
+    *ptr2=*ptr1;
+    *ptr1=temp;
+}
+int main(void){
+    int num1=10,num2=20,num3=30;
+    printf("num1: %d,num2: %d,num3: %d\n",num1,num2,num3);
+    Swap3(&num1,&num2,&num3);
+    printf("num1: %d,num2: %d,num3: %d\n",num1,num2,num3);
+}
