fb35eb1865863ee548a6352c9035719cf0143882
junes7 <2junes@naver.com>
2021-10-10 09:06:59 +0900

    solve that convert decimal to binary number problem by using while loop, and scanf, printf function

---
+++ C_Programming/Part2_start of Pointer and array/Chapter15/Q2.c
@@ -0,0 +1,13 @@
+// convert decimal to binary number
+include<stdio.h>
+int main(void){
+    int num,t=1,s=0;
+    printf("10진수 정수 입력: ");
+    scanf("%d",&num);
+    while(num!=0){
+        s+=num%2*t;
+        num/=2;
+        t*=10;
+    }
+    printf("%d\n",s);
+}
