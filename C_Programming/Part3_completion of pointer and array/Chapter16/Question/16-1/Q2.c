50295dfb5930f9c037a609011ef56c58f9923ec6
junes7 <2junes@naver.com>
2021-10-12 07:52:48 +0900

    study how to transpose 2D matrix array

---
+++ C_Programming/Part3_completion of pointer and array/Chapter16/Question/16-1/Q2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(void){
+    int i,j,k=1,arr1[4][2];
+    int arr[2][4]={
+        {1, 2, 3, 4},
+        {5, 6, 7, 8}
+    };
+    for(i=0;i<2;i++){
+        for(j=0;j<4;j++)
+           arr1[j][i]=arr[i][j];
+    }
+    for(i=0;i<4;i++){
+        for(j=0;j<2;j++){
+            printf("%d ",arr1[i][j]);
+        }
+        printf("\n");
+    }
+}
