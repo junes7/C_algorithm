91e2013a8e41a5ab0c90ad23dbf548a08b101e96
junes7 <2junes@naver.com>
2021-10-17 06:45:05 +0900

    study how to define and use puts and fputs function with printf function

--- C_Programming/Part4_deep understanding of C language/Chapter21/21-3/WrtieString.c
+++ C_Programming/Part4_deep understanding of C language/Chapter21/21-3/WrtieString.c
@@ -1,13 +1,12 @@
 #include<stdio.h>
 int main(void){
+    // puts와 fputs의 차이를 알 수 있다.
     char *str="Simple String";
     printf("1. puts test ------\n");
     puts(str);
     puts("So Simple String");
-
     printf("2. fputs test ------\n");
     fputs(str,stdout);printf("\n");
     fputs("So Simple String",stdout);printf("\n");
     printf("3. end of main ------\n");
-    return 0;
 }
