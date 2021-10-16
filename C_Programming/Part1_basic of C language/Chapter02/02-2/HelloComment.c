25b3aff130661f7fe46d6ef4de4ffeafb8809ded
junes7 <2junes@naver.com>
2021-10-17 08:46:11 +0900

    review how to use comment in C with printf function

---
+++ C_Programming/Part1_basic of C language/Chapter02/02-2/HelloComment.c
@@ -0,0 +1,19 @@
+/*
+title: print Hello World
+function: print string
+filename: HelloComment.c
+date of revision: 2014.07.15
+writer: À±¼º¿ì
+*/
+// define header file
+#include<stdio.h>
+// start main function
+int main(void){
+    /*
+    In this function, print one string.
+    String is printed on the monitor.
+    */
+    //print string    
+   printf("Hello World!\n");
+   return 0;
+}
