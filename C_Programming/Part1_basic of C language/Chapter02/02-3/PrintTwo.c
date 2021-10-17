10d94e457925b79ed5f069e5c79dfb2ff63d60e9
junes7 <2junes@naver.com>
2021-10-17 09:01:40 +0900

    review how to do various combinations of print patterns with printf function

---
+++ C_Programming/Part1_basic of C language/Chapter02/02-3/PrintTwo.c
@@ -0,0 +1,7 @@
+// how to do various combinations of print pattern
+#include<stdio.h>
+int main(void){
+    printf("My age: %d\n",20);
+    printf("%d is my point.\n",100);
+    printf("Good \nmorning \neverybody\n");
+}
