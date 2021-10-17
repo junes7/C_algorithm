bd851353225c4052a3336ee8e6e0db26b9241ba6
junes7 <2junes@naver.com>
2021-10-17 09:08:47 +0900

    solve print string and integer problems with printf function

---
+++ C_Programming/Part1_basic of C language/Chapter02/Question/02-2/Q1.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(void){
+    printf("제 이름은 %s입니다.\n","홍길동");
+    printf("제 나이는 %d살이고요.\n",20);
+    printf("제가 사는 곳의 번지수는 %d-%d입니다.\n",123,456);
+}
---
+++ C_Programming/Part1_basic of C language/Chapter02/Question/02-2/Q2.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+int main(void){
+    printf("%dX%d=%d\n",4,5,4*5);
+    printf("%dX%d=%d\n",7,9,7*9);
+}
