a09535bb8f94b5799841a4d04a536d158fb9d108
junes7 <2junes@naver.com>
2021-11-05 06:05:31 +0900

    review how to use sign operator in C

---
+++ C_Programming/Part1_basic of C language/Chapter03/03-2/OperatorThree.c
@@ -0,0 +1,13 @@
+// 2, -4를 -2, 4로 변환
+#include<stdio.h>
+int main(void){
+    int num1=2, num2=-4;
+    num1 = -num1;
+    num2 = -num2;
+    printf("num1: %d\n", num1);
+    printf("num2: %d\n", num2);
+    return 0;
+}
+
+
+
