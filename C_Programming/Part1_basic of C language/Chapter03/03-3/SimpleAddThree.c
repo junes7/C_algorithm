6ba2e49a8d709f8574ec8f12cece45ab2036eb35
junes7 <2junes@naver.com>
2021-11-06 07:27:44 +0900

    review how to apply scanf function in C

---
+++ C_Programming/Part1_basic of C language/Chapter03/03-3/SimpleAddThree.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(void){
+    int num1, num2, result;
+    printf("정수 one: ");
+    scanf("%d", &num1);
+    printf("정수 two: ");
+    scanf("%d", &num2);
+    result=num1+num2;
+    printf("%d + %d = %d\n", num1, num2, result);
+    return 0;
+}
