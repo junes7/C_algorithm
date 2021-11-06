7bb12bae95befb5aca5b30bb1e6d68f53ed8dba1
junes7 <2junes@naver.com>
2021-11-07 07:44:57 +0900

    solve the square of an integer problem by using multiplication operator

---
+++ C_Programming/Part1_basic of C language/Chapter03/Question/03-1/Q3.c
@@ -0,0 +1,9 @@
+// the square of an integer
+#include<stdio.h>
+int main(void){
+    int num;
+    printf("하나의 정수 입력: ");
+    scanf("%d", &num);
+    printf("%d의 제곱 결과: %d\n", num, num*num);
+    return 0;
+}
